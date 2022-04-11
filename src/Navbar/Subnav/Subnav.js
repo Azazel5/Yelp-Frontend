a7abd9468f2499b48c57a4c316199511dbbdc8cc
Subhanga Upadhyay <subhanga2013@gmail.com>
2022-04-11 18:16:27 -0500

    Finished the sub-navigation menu
    - More styling and bringing things over from bulma css
    - More overriding bulma with :global and specific classNames

--- src/Navbar/Subnav/Subnav.js
+++ src/Navbar/Subnav/Subnav.js
@@ -1,14 +1,34 @@
 import React from 'react'
-// import styles from './Subnav.module.css'
+import styles from './Subnav.module.css'
 import SubnavItem from './SubnavItem/SubnavItem'
 
 export default function Subnav() {
     return (
-        <div>
-            <SubnavItem label="Restaurant" icon="fa-utensils"/>
-            <SubnavItem label="Home Services" icon="fa-home"/>
-            <SubnavItem label="Auto Services" icon="fa-car-side"/>
-            <SubnavItem label="More" icon="fa-info-circle" showRightBorder/>
+        <div className={styles.container}>
+            <div className={styles['sub-nav']}>
+                <div>
+                    <SubnavItem label="Restaurant" icon="fa-utensils" />
+                    <SubnavItem label="Home Services" icon="fa-home" />
+                    <SubnavItem label="Auto Services" icon="fa-car-side" />
+                    <SubnavItem label="More" icon="fa-info-circle" showRightBorder />
+                </div>
+
+                <div>
+                    <button className={`button ${styles['subnav-button']} ${styles['omit-right-border']}`}>
+                        <span className="icon">
+                            <i className="fas fa-pen" />
+                        </span>
+                        <span>Write a review</span>
+                    </button>
+
+                    <button className={`button ${styles['subnav-button']}`}>
+                        <span className="icon">
+                            <i className="fas fa-hotel" />
+                        </span>
+                        <span>For businesses</span>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
