2f5cfcd600cd1116d204dd628d7e58e14ea0b5a5
Azazel5 <subhanga2013@gmail.com>
2022-04-13 12:08:18 -0500

    Added a rating component (react-ratingy)

---
+++ src/BusinessRating/BusinessRating.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Rating from 'react-rating'
+import styles from './BusinessRating.module.css'
+
+export default function BusinessRating() {
+    return (
+        <div className={styles.rating}>
+            <Rating 
+                emptySymbol="far fa-star"
+                fullSymbol="fas fa-star"
+                fractions={2}
+                readonly
+                initialRating={3}
+            />
+        </div>
+    )
+}
--- src/Search/SearchResults/SearchResult/SearchResult.js
+++ src/Search/SearchResults/SearchResult/SearchResult.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import BusinessRating from '../../../BusinessRating/BusinessRating'
 import styles from './SearchResult.module.css'
 
 export default function SearchResult() {
@@ -7,13 +8,13 @@ export default function SearchResult() {
             <img className={styles['business-image']} src="https://via.placeholder.com/210" alt="business info" />
             <div className={styles['business-info']}>
                 <h2 className='subtitle'>Burger Place</h2>
-                <p>Rating</p>
+                <BusinessRating />
                 <p>
                     $$ 
                     <span className='tag'>Burgers</span> <span className='tag'>Fast Food</span>
                 </p>
             </div>
-            <div>
+            <div className={styles['contact-info']}>
                 <p>+1 123456789</p>
                 <p>123 Northeast Ave</p>
                 <p>441114, Kathmandu, Nepal</p>
