f297568f87c7df3f514bcb6954df63bc7357319b
Azazel5 <subhanga2013@gmail.com>
2022-04-13 17:40:29 -0500

    Built the search functionality on the homepage
    - Created states to hold form values
    - onSubmit and onClick of the search button, redirect to the search component
    - encodeURL is a built in JS function for encoding strings

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -2,11 +2,12 @@ import React from 'react'
 import styles from './Navbar.module.css'
 import logo from '../assets/logo.png'
 import SearchBar from '../SearchBar/SearchBar'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
         <div className={styles['nav-bar']}>
-            <img src={logo} className={styles.logo} alt='belb logo' />
+            <Link to="/"><img src={logo} className={styles.logo} alt='belb logo' /></Link>
             <SearchBar small />
             <button className={`button ${styles['nav-button']}`}>Sign In</button>
             <button className={`button ${styles['nav-button']}`}>Register</button>
--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,32 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SearchBar.module.css'
 
 export default function SearchBar(props) {
+    const [term, setTerm] = useState(props.term || '')
+    const [location, setLocation] = useState(props.location || '')
+
     const sizeClass = props.small ? '' : 'is-medium'
-    
+
+    function onSubmit(e) {
+        e.preventDefault()
+
+        if (typeof (props.search) === 'function') {
+            props.search(term, location)
+        }
+    }
+
     return (
-        <div className="field has-addons" style={{marginBottom: '0'}} >
-            <p className="control">
-                <button className={`button is-static ${sizeClass}`}>
-                    Search
-                </button>
-            </p>
-            <p className="control">
-                <input className={`input ${sizeClass} ${styles['input-control']}`} type="text" placeholder="burgers, barbers, spas, handymen" />
-            </p>
-            <p className="control">
-                <button className={`button is-static ${sizeClass}`}>
-                    NEAR
-                </button>
-            </p>
-            <p className="control">
-                <input className={`input ${sizeClass} ${styles['input-control']}`} type="text" placeholder="Where" />
-            </p>
-            <div className={`button ${sizeClass} ${styles['search-button']}`}>
-                <span className={`icon is-small ${styles['search-icon']}`}>
-                    <i className="fas fa-search"></i>
-                </span>
+        <form onSubmit={onSubmit}>
+            <div className="field has-addons" style={{ marginBottom: '0' }} >
+                <p className="control">
+                    <button className={`button is-static ${sizeClass}`}>
+                        Search
+                    </button>
+                </p>
+                <p className="control">
+                    <input className={`input ${sizeClass} ${styles['input-control']}`}
+                        type="text" placeholder="burgers, barbers, spas, handymen"
+                        onChange={(e) => setTerm(e.target.value)} />
+                </p>
+                <div className="control">
+                    <div className={`button is-static ${sizeClass}`}>
+                        NEAR
+                    </div>
+                </div>
+                <p className="control">
+                    <input className={`input ${sizeClass} ${styles['input-control']}`}
+                        type="text" placeholder="Where"
+                        onChange={(e) => setLocation(e.target.value)} />
+                </p>
+                <div className={`button ${sizeClass} ${styles['search-button']}`} onClick={onSubmit}>
+                    <span className={`icon is-small ${styles['search-icon']}`}>
+                        <i className="fas fa-search"></i>
+                    </span>
+                </div>
             </div>
-        </div>
+        </form>
     )
 }
