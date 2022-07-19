ef0fe6d65fce238118b6f55d9b0e0481aaa16d79
Azazel5 <subhanga2013@gmail.com>
2022-07-19 17:15:05 -0500

    Finishing up the application
    - Added a loading spinner for the API response
    - Update the URL params on search (for subsequent searches after the first one)

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -5,16 +5,25 @@ import Subnav from '../Navbar/Subnav/Subnav'
 import SearchResults from './SearchResults/SearchResults'
 import SearchResultsSummary from './SearchResultsSummary/SearchResultsSummary'
 import { useBusinessSearch } from '../hooks/yelp-api/useBusinessSearch'
+import { useNavigate } from 'react-router-dom';
 
 export default function Search() {
     const location = useLocation()
+    const navigate = useNavigate()
     const params = new URLSearchParams(location.search)
     const term = params.get('find_desc')
     const locationParam = params.get('find_loc')
 
     const [businesses, amountResults, searchParams, performSearch] = useBusinessSearch(term, locationParam)
 
+    if (!term || !locationParam) {
+        navigate('/')
+    }
+
     function search(term, location) {
+        const urlEncodedTerm = encodeURI(term)
+        const urlEncodedLocation = encodeURI(location)
+        navigate(`/search?find_desc=${urlEncodedTerm}&find_loc=${urlEncodedLocation}`)
         performSearch({ term, location })
     }
 
--- src/Search/SearchResults/SearchResults.js
+++ src/Search/SearchResults/SearchResults.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import styles from './SearchResults.module.css'
 import SearchResult from './SearchResult/SearchResult'
+import Spinner from '../../Spinner/Spinner'
 
 export default function SearchResults(props) {
-    const {businesses} = props
+    const { businesses } = props
+    let searchResults = <Spinner />
 
-    if (!businesses || !businesses.length) {
-        return (<div></div>)
+    if (businesses && businesses.length) {
+        searchResults = businesses.map(business => (
+            <SearchResult key={business.id} business={business} />
+        ))
     }
-
-    const searchResults = businesses.map(business => (
-        <SearchResult key={business.id} business={business} />
-    ))
 
     return (
         <div className={styles['search-results']}>
